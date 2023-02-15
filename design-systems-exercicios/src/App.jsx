b498c4ee56d9158f9b652375e97e6a4bed7b62b4
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-02-15 18:50:54 -0300

    exercicios-concluidos

--- design-systems-exercicios/src/App.jsx
+++ design-systems-exercicios/src/App.jsx
@@ -1,30 +1,40 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Flex } from '@chakra-ui/react'
 import Card from './components/Card'
 import { useState } from 'react'
 
 
 function App() {
 
-const produtos = [{
-  marca: "Rafael Cadeiras",
-  nome: "Cadeira",
-  precoComDEsconto: "20,00",
-  precoNormal: "60,00"
-}] 
+  const produtos = [{
+    marca: "Rafael Cadeiras",
+    nome: "Cadeira",
+    precoComDEsconto: "20,00",
+    precoNormal: "60,00"
+  },
+  {
+    marca: "Rafael Cadeiras",
+    nome: "Cadeira",
+    precoComDEsconto: "20,00",
+    precoNormal: "60,00"
+  }
+  ]
 
   const [produtosList, setProdutos] = useState(produtos)
 
   return (
 
     <ChakraProvider resetCSS>
-      {produtosList && produtosList.map((produto) => {
-        return <Card
-        marca={produto.marca}
-        nome={produto.nome}
-        precoComDEsconto={produto.precoComDEsconto}
-        precoNormal={produto.precoNormal}
-        />
-      })}
+      <Flex justify={'center'}>
+        {produtosList && produtosList.map((produto) => {
+          return <Card
+            marca={produto.marca}
+            nome={produto.nome}
+            precoComDEsconto={produto.precoComDEsconto}
+            precoNormal={produto.precoNormal}
+          />
+        })}
+
+      </Flex>
 
     </ChakraProvider>
 
